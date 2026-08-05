abfef9cbebdd96feb34430560f0e54028f197f7c
skhan397143-cloud <skhan397143@gmail.com>
2026-08-05 03:56:23 -0700

    Update app.py

--- app.py
+++ app.py
@@ -802,15 +802,16 @@ if st.button("🧠 Analyze with AI Brain"):
 
             User Question:
             {user_question}
-
+            """
     response = g4f.ChatCompletion.create(
     model="gpt-4o-mini",
-    messages=f"{prompt}"
-)
+    messages=[
+    {"role": "user", "content": prompt}
+]
 
-   st.write(response)
+    st.write(response)
 
-   Answer like a professional Wall Street analyst.
+    Answer like a professional Wall Street analyst.
     """
 
    
